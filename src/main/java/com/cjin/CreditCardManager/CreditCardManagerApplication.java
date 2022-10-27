a0fec782abb2bdeab52dbf3c238a3e7a7a1a46af
Christopher Jin <chrisjin1@gmail.com>
2022-10-26 21:49:29 -0400

    first commit

---
+++ src/main/java/com/cjin/CreditCardManager/CreditCardManagerApplication.java
@@ -0,0 +1,14 @@
+package com.cjin.CreditCardManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardManagerApplication.class, args);
+		System.out.println("Hello, World");
+	}
+
+}
---
+++ src/test/java/com/cjin/CreditCardManager/CreditCardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjin.CreditCardManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
