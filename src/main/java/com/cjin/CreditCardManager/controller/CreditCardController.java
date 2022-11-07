a1ea17bf414924a9d59c01759bea9e5eb2ab9741
Christopher Jin <chrisjin1@gmail.com>
2022-11-06 22:17:17 -0500

    added todos for controller

--- src/main/java/com/cjin/CreditCardManager/controller/CreditCardController.java
+++ src/main/java/com/cjin/CreditCardManager/controller/CreditCardController.java
@@ -1,4 +1,17 @@
 package com.cjin.CreditCardManager.controller;
 
+import com.cjin.CreditCardManager.interfaces.CreditCardRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CreditCardController {
+    private final CreditCardRepository cCRepository;
+
+    public CreditCardController(CreditCardRepository cCRepository) {
+        this.cCRepository = cCRepository;
+    }
+
+    //Get Mapping --> return all credit cards
+    //Post Mapping --> New Credit card
+    //
 }
